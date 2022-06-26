ff76b33aec0f41a3a01b9632b1068afdb593636d
Jay1dcoder <105127281+Jay1dcoder@users.noreply.github.com>
2022-06-26 01:08:16 +0100

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delux Hotels</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<?php
+?>
+<body>
+
+<div class="header">
+
+<span class="logo"> <b>DELUX HOTEL</b></span>
+
+        <nav>
+            <a href="" class="links">Home</a>
+            <a href="" class="links">Booking</a>
+            <a href="contact.php" class="links">Contact Us</a>
+            <a href="" class="links">Location</a>
+
+            <!-- <div class="dropdown">
+                <button class="dropbtn">Account</button>
+                <div class="dropdown-content"> -->
+                <a href="" class="links" >Sign Up</a>
+                <a href="" class="links" >Login</a>
+                <!-- </div>
+            </div> -->
+        </nav>
+
+</div>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+    <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1" ></li>
+        <li data-target="#demo" data-slide-to="2" ></li>
+        <li data-target="#demo" data-slide-to="3" ></li>
+        <!-- <li data-target="#demo" data-slide-to="4" ></li> -->
+    </ul>
+
+    <div class="carousel-inner" style="z-index:-1;">
+
+
+      <div class="carousel-item active">
+            <img src="./Images/fernando-alvarez-rodriguez-M7GddPqJowg-unsplash.jpg" alt="" width="100%" height="auto">
+        </div>
+
+    <div class="carousel-item ">
+    <img src="./Images/alexander-kaunas-xEaAoizNFV8-unsplash.jpg" alt="" width="100%" height="auto">
+    </div>
+
+    
+    <!-- <div class="carousel-item ">
+    </div> -->
+
+    
+    <div class="carousel-item ">
+        <img src="./Images/ralph-ravi-kayden-FqqiAvJejto-unsplash.jpg" alt="" width="100%" height="auto">
+    </div>
+
+    
+    <div class="carousel-item ">
+        <img src="./Images/vojtech-bruzek-Yrxr3bsPdS0-unsplash.jpg" alt="" width="100%" height="auto">
+    </div>
+
+    <!-- <a href="#demo" class="carousel-control-prev" data-slide="prev" style="cursor: pointer;margin-top:45%;">
+        <span class="carousel-control-prev-icon" ></span>
+    </a>
+
+    <a href="#demo" class="carousel-control-next" data-slide="next" style="cursor: pointer;margin-top:45%;">
+        <span class="carousel-control-next-icon"></span>
+    </a> -->
+
+    </div>
+</div>
+
+<?php
+    require_once("contacthome.php");
+?>
+</body>
+</html>
