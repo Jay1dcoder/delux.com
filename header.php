c9922c6a0550f8985998410f9b80f3e78391c9e0
Jay1dcoder <105127281+Jay1dcoder@users.noreply.github.com>
2022-08-02 16:24:03 +0100

    Update header.php

--- header.php
+++ header.php
@@ -28,16 +28,3 @@
         </nav>
 
 </div>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Delux Hotels</title>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <link rel="stylesheet" href="about.css">
-    <script src="https://kit.fontawesome.com/af621931a1.js" crossorigin="anonymous"></script>
-    
-</head>
-<body>
